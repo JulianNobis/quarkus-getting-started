7b2c7325043c1a5e67b26f0be5364e43fb9a739f
JulianNobis <julian09022001@gmail.com>
2019-09-24 08:23:00 +0200

    .

---
+++ greetingsResponse/src/main/java/at/htl/quickstart/GreetingService.java
@@ -0,0 +1,10 @@
+package at.htl.quickstart;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class GreetingService {
+    public String greeting(String name){
+        return "hello " + name + "\n";
+    }
+}
